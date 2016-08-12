552713dd0c0c717a4d4e8a078872bd73df3a3493
LiTianjue <mhpmii@126.com>
2016-08-12 16:53:18 +0800

    添加一个并发测试脚本

---
+++ NfcIdVerify/verify_client/auto_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ok=0
+bad=0
+count=1000
+aver=0
+for((i=0; i <1000;i++))
+do
+	./nfc_client -h 172.16.2.198 -p 9019 -s 201503150003 $2>/dev/null
+	if [ $? == 0 ]
+	then
+		ok=$(($ok+1))
+	else
+		bad=$(($bad+1))
+	fi
+done
+
+echo "Cost ${SECONDS}"
+echo "Success ${ok} Fail ${bad}"
+
+echo "--------------------------"
+num=`echo "sclae=2; ${count} / ${SECONDS}" | bc`
+echo "Average ${num} times / s"
