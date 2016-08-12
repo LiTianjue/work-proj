e43dad2e607069309837b655504c7987a2163c64
andy <mhpmii@126.com>
2016-08-12 11:09:11 +0800

    修改测试程序

--- NfcIdVerify/verify_client/test.cpp
+++ NfcIdVerify/verify_client/test.cpp
@@ -4,29 +4,58 @@
 #include <unistd.h>
 #include "verify_client.h"
 
+void print_help()
+{
+	printf(" --usage: --\n");
+	printf("--build time:%s--%s\n",__TIME__,__DATE__);
+	printf(" -h  server ip addr \n");
+	printf(" -p  server port \n");
+	printf(" -s  client serial number.\n");
+}
+
 
 
 int main(int argc,char **argv)
 {
+	int opt;
 	char ip[32] ={0};
 	int  port = 0;
 	char serial[32] = {0};
 
-
-	printf("Input Server IP:");
-	scanf("%s",ip);
-	
-	printf("Input Server Port:");
-	scanf("%d",&port);
-
-
-	printf("Input Test Serial:");
-	scanf("%s",serial);
+	if(argc <=1)
+	{
+		printf("Input Server IP:");
+		scanf("%s",ip);
+		printf("Input Server Port:");
+		scanf("%d",&port);
+		printf("Input Test Serial:");
+		scanf("%s",serial);
+	}else
+	{
+		while((opt = getopt(argc,argv,"h:p:s:")) != -1)
+		{
+			switch(opt)
+			{
+				 case 'h':
+					strcpy(ip,optarg);
+					break;
+				case 'p':
+					port = atoi(optarg);
+					break;
+				case 's':
+					strcpy(serial,optarg);
+					break;
+				default:
+					print_help();
+				 	return 1;
+			}
+		}
+	}
 
 
 	int ok = 0;
 	int bad = 0;
-	while(1)
+	while(0)
 	{
 		if(verify_serial(ip,port,serial,strlen(serial),2) < 0)
 			bad+=1;
@@ -36,9 +65,20 @@ int main(int argc,char **argv)
 		sleep(1);
 
 	}
+	
 
+	if(verify_serial(ip,port,serial,strlen(serial),2) < 0)
+	{
+		printf("Verify [%s] Failed \n",serial);
+	
+		return 1;
+	}
+	else
+	{
+		printf("Verify [%s] Success.\n",serial);
 
-	return 0;
+		return 0;
+	}
 
 
 
