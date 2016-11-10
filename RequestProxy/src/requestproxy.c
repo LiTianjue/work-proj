7d3985e19fd07f5642faf9fba6a0b95523c891f9
LiTianjue <mhpmii@126.com>
2016-11-10 17:48:34 +0800

    update

---
+++ RequestProxy/src/requestproxy.c
@@ -0,0 +1,39 @@
+
+//#include "common.h"
+
+
+
+
+
+#define DOTEST	1
+
+
+/*
+ *  初始化选择,配置文件处理
+ *
+ * */
+static void
+serverinit(int argc,char *argv[]);
+
+/*
+ * 打印使用说明
+ *
+ **/
+static void
+usage(int code);
+
+/*
+ *	显示版本信息
+ *
+ */
+static void 
+showversion(const int level);
+
+int main(int argc,char *argv[]);
+
+int
+main(int argc,char *argv[])
+{
+	printf("hello \n");
+}
+
